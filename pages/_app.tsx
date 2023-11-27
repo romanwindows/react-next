ad1ba3b6a3098f297341ccb0fad4bf618739d167
romanwindows <esportstats0@gmail.com>
2023-11-27 17:47:00 +0200

    part07lesson01-fonts-and-colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>My top - наш лучший топ</title>
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
