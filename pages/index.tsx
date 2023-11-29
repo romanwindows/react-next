591a0b56d7847b5fced8a77ce3d130d8953decf7
romanwindows <esportstats0@gmail.com>
2023-11-29 20:29:47 +0200

    part07lesson03-classnames

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import { Htag } from '../components';
+import { Button, Htag } from '../components';
 
 export default function Home(): JSX.Element {
   return (
     <div>
       <Htag tag="h1">TEXT</Htag>
+      <Button appearance="primary">Кнопка</Button>
+      <Button appearance="ghost">Кнопка</Button>
     </div>
   );
 }
